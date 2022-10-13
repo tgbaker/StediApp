6e3cf9f02a1b47edda47fd6a7eb6094602f689da
tgbaker <tgbaker@byui.edu>
2022-10-13 13:45:59 -0600

    added actual stuff to sendtext.js

---
+++ components/SendText.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, TextInput, Button } from "react-native";
+
+const UselessTextInput = () => {
+  const [text, onChangeText] = React.useState("Useless Text");
+  const [number, onChangeNumber] = React.useState(null);
+
+  return (
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <Button
+        title="Press me"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="useless placeholder"
+        keyboardType="numeric"
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
+
+export default UselessTextInput;
